39f9e0fb1763c0ae11f29d6691b2685c00948018
DzmitryLamaka <71657204+DzmitryLamaka1995@users.noreply.github.com>
2021-01-16 15:19:15 +0300

    Delete java-developer-tasks/src/by/htp directory

--- java-developer-tasks/src/by/htp/fundamentals1/main/Task12.java
+++
@@ -1,1 +0,0 @@
-package by.htp.fundamentals1.main;public class Task12 {	public static void main(String[] args) {		// 12. Вывести на экран соответствий между символами и их численными		// обозначениями в		// памяти компьютера (Таблицу ASCII).		char ch;		for (int i = 0; i < 256; i++) {			ch = (char) (i);			System.out.println("#" + i + " - " + ch);		}	}}
